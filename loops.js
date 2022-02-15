52c7bf3f81bd49bbf9980d0a95616e762b69f609
pedrolibarty <pedrolibarty@outlook.com>
2022-02-15 14:48:48 -0300

    create: exemplos

---
+++ loops.js
@@ -0,0 +1,90 @@
+/*
+const frutas = ['banana', 'maça', 'abacate']
+
+let i = 0
+
+while (i<frutas.length) {
+    console.log(frutas[i])
+
+    console.log(i)
+
+    i=i+1
+} 
+*/
+/*
+let i = 0
+
+while (i<=15) {
+    console.log(i)
+
+    i=i+1
+}
+*/
+
+/*
+const list = ['a', 'b', 'c' ]
+
+let i = 0
+
+do {
+    console.log(list[i])
+
+    console.log(i)
+
+    i= i+1
+} while (i < list.length)*/
+
+/*const n=5;
+
+for(let i=1; i <= n; i++) {
+    console.log(i);
+}*/
+/*
+let i = 0;
+while(i<=10){
+  console.log(i)
+  i++;
+}
+*/
+/*
+const frutas = ['banana', 'maça', 'abacate']
+
+let frases =[' estava uma delícia.', ' estava velha.', ' não estava madura']
+
+let i = 0
+
+while (i<frutas.length) {
+    console.log('A fruta ' + frutas[i] + frases[i])
+
+    console.log(i)
+
+    i=i+1
+} 
+*/
+/*
+let redesSociais = ['Facebook', 'Twitter', 'Instagram', 'Dev community', 'Linkedin', 'reddit', 'WhatsApp', 'Snapchat', 'Tik tok'];
+
+let i = 0;
+while (i<redesSociais.length){
+  console.log(redesSociais[i]);
+  i++;
+}*/
+/*
+function mostrandoNoConsole(arr){
+    let i = 0;
+    while(i < arr.length){
+      console.log(arr[i])
+      i++
+    }
+  }
+  
+  mostrandoNoConsole(['banana', 'maça', 'abacate'])*/
+
+  function loopingWhile(num){
+    while(num > 10){
+        console.log(num)
+      num++
+    }
+  }
+  
+  loopingWhile(num)
